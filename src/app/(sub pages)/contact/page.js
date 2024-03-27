d68cde180429c850f76a862de8e1bf7623392b69
Doston1232696 <otkurovdoston69@gmail.com>
2024-03-27 15:11:57 +0500

    first commit

---
+++ src/app/(sub pages)/contact/page.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import bg from "../../../../public/background/contact-background.png";
+import Form from "@/components/contact/Form";
+
+export const metadata = {
+  title: "Contact",
+};
+
+export default function Contact() {
+  return (
+    <>
+      <Image
+        src={bg}
+        alt="Next.js Portfolio website's contact page background image"
+        priority
+        sizes="100vw"
+        className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
+      />
+
+      <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
+        <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
+          <h1 className="text-accent font-semibold text-center text-4xl capitalize">
+            summon the wizard
+          </h1>
+          <p className="text-center font-light text-sm xs:text-base">
+            NMADUR KERAKMI MENGA BOGLANING
+          
+          </p>
+        </div>
+        <Form />
+      </article>
+    </>
+  );
+}
---
+++ src/app/data.js
@@ -0,0 +1,130 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "TomatoFood",
+    description: "Food Delevery Your Home",
+    date: "2023-08-15",
+    demoLink: "https://jovohirga-proyekt.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Online Read Book",
+    description: "Read For All Day",
+    date: "2023-06-20",
+    demoLink: "https://book-website-nextjs-main.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Shopper",
+    description: "Plan and track expenses",
+    date: "2023-09-10",
+    demoLink: "https://ai-builder.mobirise.com/pages/KR3mCrk5s_ofs_mYBL5bD#",
+  },
+  {
+    id: 4,
+    name: "AIRBNB",
+    description: "Order Beautiful Home",
+    date: "2023-05-30",
+    demoLink: "https://doston-aribnb.vercel.app/",
+  },
+  {
+    id: 5,
+    name: "LAtinka",
+    description: "Online Hudi SHope Uz",
+    date: "2023-07-12",
+    demoLink: "https://65b680f8c23cc3835a29b9df--dazzling-lamington-de497e.netlify.app/",
+  },
+  {
+    id: 6,
+    name: "Iphone ",
+    description: "Log your travels",
+    date: "2024-02-28",
+    demoLink: "https://iphone-main-lime.vercel.app/",
+  },
+  {
+    id: 7,
+    name: "StudyBuddy",
+    description: "Collaborative learning platform",
+    date: "2024-03-05",
+    demoLink: "https://studybuddy.example.com",
+  },
+  {
+    id: 8,
+    name: "TechTalk",
+    description: "Tech news aggregator",
+    date: "2024-03-25",
+    demoLink: "https://techtalk.example.com",
+  },
+  {
+    id: 9,
+    name: "FitTrack",
+    description: "Fitness and workout tracker",
+    date: "2024-03-26",
+    demoLink: "https://fittrack.example.com",
+  },
+  {
+    id: 10,
+    name: "MindfulMoments",
+    description: "Meditation and mindfulness app",
+    date: "2024-03-27",
+    demoLink: "https://mindfulmoments.example.com",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/Doston1232696",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/doston-otkurov-a686212a8/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://twitter.com/d_otkurov",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Instagram",
+    link: "https://instagram.com/dostonck_me?igshid=NzZlODBkYWE4Ng==",
+    icon: "twitter",
+    newTab: true,
+  },
+
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
