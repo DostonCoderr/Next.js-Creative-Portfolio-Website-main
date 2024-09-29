743bd35531d6b559f77b26cf83d42dc2e9de0f44
Doston <132774658+DostonCoderr@users.noreply.github.com>
2024-09-29 20:59:19 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
+    <meta name="google-site-verification" content="dznEGAcVnJar9UKlznB9EXwv20RNls8rb-AXHhSl2iY" />
     <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N79TVN5G');`,
